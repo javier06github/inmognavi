e78f4cafb041ad2fd60c06622ace715f92848be7
javiergit <licrissojavier@gmail.com>
2024-10-27 13:27:38 -0300

    primero

---
+++ propiedades.js
@@ -0,0 +1,76 @@
+const propiedades = [
+    {
+        id: 1,
+        titulo: "Lote Barrio Paraiso",
+        ubicacion: "Moquehua",
+        tipo: "Lote",
+        precio: "Consultar",
+        imagen: "img/Captura de pantalla 2024-09-22 153149.png",
+        fotos: [
+            "img/lotep1.png",
+            "img/lotep2.png",
+            "img/Captura de pantalla 2024-09-22 153149.png"
+        ],
+        descripcion: "Ubicación céntrica con vistas espectaculares. Genial esquina.",
+        metrosCubiertos: 0,
+        metrosLote: "30 x 28",
+        direccion: "Calle Falsa 123, Ciudad, País",
+        iframeMapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1372.5787430299802!2d-59.77058302880161!3d-35.092704700675405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bc265eb631519f%3A0x3f5678ec6c1b9f4d!2sMoquehu%C3%A1%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1727308585826!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+    },
+    {
+        id: 2,
+        titulo: "Casa Familiar",
+        ubicacion: "Moquehua",
+        tipo: "Casa",
+        precio: "Consultar",
+        imagen: "img/Captura de pantalla 2024-09-22 153326.png",
+        fotos: [
+            "img/Casa2.png",
+            "img/Casa3.png",
+            "img/Captura de pantalla 2024-09-22 153326.png"
+        ],
+        descripcion: "Amplia casa en zona residencial tranquila, perfecta para familias que buscan comodidad y seguridad. Sobre asfalto. Todos los servicios",
+        metrosCubiertos: 100,
+        metrosLote: "10 x 40",
+        direccion: "25 de Mayo, B6625 Moquehuá, Provincia de Buenos Aires",
+        iframeMapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1632.2865692656242!2d-59.77419882108488!3d-35.092396849712316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bc265e131b4e15%3A0x99d8dc5a3ee15a46!2s25%20de%20Mayo%2C%20B6625%20Moquehu%C3%A1%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1727309828372!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+    },
+    
+    {
+        id: 3,
+        titulo: " 2 Lotes en inmejorable esquina",
+        ubicacion: "Moquehua",
+        tipo: "Lote",
+        precio: " 23000 c/u",
+        imagen: "img/loteprop3.png",
+        fotos: [
+            "img/loteprop.png",
+            "img/loteprop (2).png",
+            "img/loteprop3.png"
+        ],
+        descripcion: "Ubicación céntrica con vistas espectaculares. Genial esquina.",
+        metrosCubiertos: 0,
+        metrosLote: "40 x 80",
+        direccion: "Calle Falsa 123, Ciudad, País",
+        iframeMapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27323.054717050517!2d-59.80052604999999!3d-35.0818125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bfb6ed9187b133%3A0xc5ffdde31f776d2!2sMoquehu%C3%A1%2C%20Buenos%20Aires%20Province!5e0!3m2!1sen!2sar!4v1662579453003!5m2!1sen!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+    },
+    {
+        id: 4,
+        titulo: "Oficinas, Casa",
+        ubicacion: "Chivilcoy",
+        tipo:  "Oficina",
+        precio: "consultar",
+        imagen: "img/Oficina.png",
+        fotos: [
+            "img/of2.png",
+            "img/of3.png"
+           
+        ],
+        descripcion: "Amplias oficinas, cochera, (puede ser casa) a 1 cuadra de la plaza .",
+        metrosCubiertos: 200,
+        metrosLote: "10 x 30",
+        direccion: "Avenida Siempre Viva 742, Ciudad, País",
+        iframeMapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3275.0720488585086!2d-58.3906!3d-34.6107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccae7a2e2f8b9%3A0x274d9f3400f8a007!2sAvenida%20Siempre%20Viva%2C%20Buenos%20Aires%20Province!5e0!3m2!1sen!2sar!4v1662579453003!5m2!1sen!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+    }
+];
+
